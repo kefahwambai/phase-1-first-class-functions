bfc7865c475746b80747ef479673e4b7063f190a
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-27 15:36:21 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+let receivesAFunction = (Come) => Come(); 
+
+
+let returnsANamedFunction = () => (function assigned() {return 'This is an assigned function'});
+
+let returnsAnAnonymousFunction = () => (function () {'This is an anonymous function'})
+
